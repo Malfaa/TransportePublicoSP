690e4e60ffa19e3f9b2ecbe0693e4e3d9db21950
gustavo <7110999@gmail.com>
2022-09-29 22:12:06 -0300

    feature & refactor: IMPORTANTE BindingAdapter.kt
    Bindings não podem estar dentro de uma classe, DEVEM PERMACER ESTÁTICOS = sem {} encapsulando.
    
    Resolução: Sem resolução

--- app/src/main/java/com/malfaa/transportepublicosp/informativo/BindingAdapter.kt
+++ app/src/main/java/com/malfaa/transportepublicosp/informativo/BindingAdapter.kt
@@ -4,25 +4,20 @@ import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.malfaa.transportepublicosp.network.models.LinhaDir
 
-class BindingAdapter {
-    
-    // TODO: acessibilidade
-
-    @BindingAdapter("linhaUm")
-    fun linhaUm (textView: TextView, linha: LinhaDir){
-        if(linha.sl == 1){
-            textView.text = linha.tp
-        }else{
-            textView.text = linha.ts
-        }
+@BindingAdapter("linhaUm")
+fun linhaUm (textView: TextView, linha: LinhaDir){
+    if(linha.sl == 1){
+        textView.text = linha.tp
+    }else{
+        textView.text = linha.ts
     }
+}
 
-    @BindingAdapter("linhaDois")
-    fun linhaDois (textView: TextView, linha: LinhaDir){
-        if(linha.sl == 1){
-            textView.text = linha.ts
-        }else{
-            textView.text = linha.tp
-        }
+@BindingAdapter("linhaDois")
+fun linhaDois (textView: TextView, linha: LinhaDir){
+    if(linha.sl == 1){
+        textView.text = linha.ts
+    }else{
+        textView.text = linha.tp
     }
 }
