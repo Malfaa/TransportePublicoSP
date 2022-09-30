bba465445aacfecb088dd6da1ecdb68afbf979ba
gustavo <7110999@gmail.com>
2022-09-29 22:02:22 -0300

    feature & refactor: MyApp utilizando Koin
    Koin como Dependency Injection foi adicionado e está operando corretamente. misc.xml teve pequenas alterações.
    
    Resolução: Sem resolução

--- app/src/main/java/com/malfaa/transportepublicosp/MyApp.kt
+++ app/src/main/java/com/malfaa/transportepublicosp/MyApp.kt
@@ -2,8 +2,10 @@ package com.malfaa.transportepublicosp
 
 import android.app.Application
 import com.malfaa.transportepublicosp.informativo.InformativoViewModel
+import com.malfaa.transportepublicosp.inicial.SplashScreenViewModel
 import com.malfaa.transportepublicosp.local.linha.LinhaDatabase
 import com.malfaa.transportepublicosp.local.onibus.OnibusDatabase
+import com.malfaa.transportepublicosp.maps.MapsViewModel
 import com.malfaa.transportepublicosp.network.SPTransApi
 import com.malfaa.transportepublicosp.repository.Repositorio
 import org.koin.android.ext.koin.androidContext
@@ -20,8 +22,12 @@ class MyApp : Application(){
 
             single { OnibusDatabase.getInstance(this@MyApp) }
             single { LinhaDatabase.getInstance(this@MyApp) }
-            single { Repositorio( get() as SPTransApi, get(), get()) }
-            viewModel{ InformativoViewModel(get() as Repositorio) }
+            single { SPTransApi }
+            single { Repositorio( get() as SPTransApi, get() as LinhaDatabase, get() as OnibusDatabase) }
+
+            viewModel { SplashScreenViewModel( get() as Repositorio) }
+            viewModel { InformativoViewModel( get() as Repositorio ) }
+            viewModel { MapsViewModel( get() as Repositorio ) }
 
         }
 
