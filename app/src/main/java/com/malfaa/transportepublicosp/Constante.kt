0b388e9db468fa664421045d544f0a4782a77b04
gustavo <7110999@gmail.com>
2022-09-29 22:17:25 -0300

    feature & refactor: Em Constante.kt var foi adicionada e imagens
    Um cons val API_key foi adicionada. Imagem de ônibus p/ os itens da lista e um refresh p/ atualizar a localização do ônibus por meio de um botão.
    
    Resolução: Sem resolução

--- app/src/main/java/com/malfaa/transportepublicosp/Constante.kt
+++ app/src/main/java/com/malfaa/transportepublicosp/Constante.kt
@@ -3,4 +3,5 @@ package com.malfaa.transportepublicosp
 object Constante {
     const val PREVISAO_TABLE_NAME = "previsao_database"
     const val LINHA_TABLE_NAME = "linha_database"
+    const val API_KEY = "a"
 }
